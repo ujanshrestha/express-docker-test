549292f3fd91e5fec760fac09e452a80059fa52e
Ujan Shrestha <ujanshrestha22@gmail.com>
2026-08-15 12:26:18 +0545

    Change location from Pokhara to Chitwan, Nepal

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get("/", async (req, res) => {
     const c = data.current;
 
     res.render("weather", {
-      location: "Pokhara, Nepal",
+      location: "Chitwan, Nepal",
       temperature: c.temperature_2m,
       humidity: c.relative_humidity_2m,
       wind: c.wind_speed_10m,
